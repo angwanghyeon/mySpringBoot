ff9424cab1dc55e10fc7565629980d941b95e40f
gwanghyeon <agh0625@gmail.com>
2023-09-22 17:32:33 +0900

    2023년9월22일 스프링 부트 공부 안광현

---
+++ shoppingmall/src/main/java/com/keduit/controller/ThymeleafExController.java
@@ -0,0 +1,98 @@
+package com.keduit.controller;
+
+import com.keduit.dto.ItemDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class ThymeleafExController {
+
+    @GetMapping("/ex01")
+    public String thymeleafEx01(Model model){
+        model.addAttribute("data","타임리프 테스트중...");
+        return "thymeleafEX/thymeleafEx01";
+    }
+
+    @GetMapping("/ex02")
+    public String thymeleafEx02(Model model){
+        ItemDto dto = new ItemDto();
+        dto.setItemDetail("상품 상세 설명");
+        dto.setItemNm("테스트 상품1");
+        dto.setPrice(10000);
+        dto.setRegTime(LocalDateTime.now());
+        model.addAttribute("itemDto", dto);
+        return "thymeleafEx/thymeleafEx02";
+    }
+
+    @GetMapping("/ex03")
+    public String thymeleafEx03(Model model){
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for (int i=1; i<21; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setId((long)i);
+            itemDto.setItemNm("테스트 상품"+i);
+            itemDto.setItemDetail("상품 상세 설명"+i);
+            itemDto.setPrice(10000+i);
+            if(i < 11){
+                itemDto.setSellStatCd("SELL");
+            }else {
+                itemDto.setSellStatCd("SOLD_OUT");
+            }
+            itemDto.setRegTime(LocalDateTime.now());
+            itemDto.setUpdateTime(LocalDateTime.now());
+
+            //list 에 추가
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+    }
+    @GetMapping("/ex04")
+    public String thymeleafEx04(Model model){
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for (int i=1; i<21; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemNm("테스트 상품"+i);
+            itemDto.setItemDetail("상품 상세 설명"+i);
+            itemDto.setPrice(10000+i);
+            if(i < 11){
+                itemDto.setSellStatCd("SELL");
+            }else {
+                itemDto.setSellStatCd("SOLD_OUT");
+            }
+            itemDto.setRegTime(LocalDateTime.now());
+            itemDto.setUpdateTime(LocalDateTime.now());
+
+            //list 에 추가
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
+
+    @GetMapping("/ex05")
+    public String thymeleafEx05(){
+        return "thymeleafEx/thymeleafEx05";
+    }
+
+    @GetMapping("/ex06")
+    public String thymeleafEx06(String param1, String param2, Model model){
+        model.addAttribute("param1",param1);
+        model.addAttribute("param2",param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
+
+    @GetMapping("/ex07")
+    public String thymeleafEx07(){
+        return "thymeleafEx/thymeleafEx07";
+    }
+
+
+}
--- shoppingmall/src/test/java/com/keduit/repository/ItemRepositoryTest.java
+++ shoppingmall/src/test/java/com/keduit/repository/ItemRepositoryTest.java
@@ -2,6 +2,9 @@ package com.keduit.repository;
 
 import com.keduit.constant.ItemSellStatus;
 import com.keduit.entity.Item;
+import com.keduit.entity.QItem;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +16,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.xml.stream.Location;
 
 import java.time.LocalDateTime;
@@ -30,6 +36,9 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    @PersistenceContext
+    EntityManager em;
+
     @Test
     @DisplayName("상품 저장 테스트")
     public void createItemTest(){
@@ -42,23 +51,28 @@ class ItemRepositoryTest {
         item.setRegTime(LocalDateTime.now());
         item.setUpdateTime(LocalDateTime.now());
 
-        //쿼리문 작성하기
+        //쿼리문 작성 하기
         Item savedItem = itemRepository.save(item);
 
         System.out.println("savedItem = " + savedItem);
     }
 
     public void createItemList(){
-        for (int i=0; i<=10; i++){
+        for (int i=1; i<=20; i++){
             Item item = new Item();
             item.setItemNm("테스트 상품"+i);
             item.setPrice(15000+i);
             item.setItemDetail("테스트 상품 상세 정보"+i);
-            item.setItemSellStatus(ItemSellStatus.SELL);
-            item.setStockNumber(10000);
+            if (i < 11){
+                item.setItemSellStatus(ItemSellStatus.SELL);
+                item.setStockNumber(10000);
+            } else{
+                item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+                item.setStockNumber(0);
+            }
             item.setRegTime(LocalDateTime.now());
             item.setUpdateTime(LocalDateTime.now());
-            //쿼리문 작성하기
+            //쿼리문 작성 하기
             Item savedItem = itemRepository.save(item);
         }
     }
@@ -175,5 +189,50 @@ class ItemRepositoryTest {
         result.get().forEach(item -> {
             System.out.println("item = " + item);
         });
+    }
+
+    @Test
+    @DisplayName("QueryDsl 조회 테스트")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        //생성
+        QItem qItem = QItem.item;
+        //sql문 작성하듯이 만든다.
+        List<Item> itemList = queryFactory.select(qItem).from(qItem).where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%"+"테스트"+"%"))
+                .orderBy(qItem.price.desc()).fetch();
+        
+        for (Item item : itemList){
+            System.out.println("item = " + item);
+        }
+    }
+
+    @Test
+    @DisplayName("queryDsl 테스트 2번")
+    public void queryDslTest2(){
+        BooleanBuilder builder = new BooleanBuilder();
+        QItem qitem = QItem.item;
+        String itemDetail = "테스트";
+        int price = 15005;
+        String itemSellStatus = "SELL";
+
+        builder.and(qitem.itemDetail.like("%"+itemDetail+"%"));
+        builder.and(qitem.price.gt(price));
+
+        if (StringUtils.equals(itemSellStatus, ItemSellStatus.SELL)){
+            builder.and(qitem.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0,5);
+        Page<Item> result = itemRepository.findAll(builder, pageable);
+
+        System.out.println("전체 페이지 수 = " + result.getTotalPages());
+        System.out.println("조회 한 전체 상품 수 = " + result.getTotalElements());
+        System.out.println("현재 페이지 번호(number) = " + result.getNumber());
+        System.out.println("페이지 별 게시물 수(size) = " + result.getSize());
+        for (Item item : result.getContent()){
+            System.out.println("item = " + item);
+        }
     }
 }
